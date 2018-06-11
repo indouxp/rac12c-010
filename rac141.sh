5c23052d9d6f123a75534067001d3d5f119fc727
Tatsuo Indou <indou.tsystem@gmail.com>
2018-06-11 09:58:55 +0900

    add rac141.sh

---
+++ rac141.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# http://www.oracle.com/technetwork/jp/database/enterprise-edition/documentation/racdb12201-inst-linux-x64-3795525-ja.pdf
+#
+
+set -eu
+
+/sbin/udevadm control --reload-rules
+
+/sbin/start_udev
