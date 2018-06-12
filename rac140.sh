341f101f7ccb7a59c75fb309ef7f3fc936552582
root <root@c7-2.tsystem.gr.jp>
2018-06-12 13:59:24 +0900

    udpate rac140.sh

--- rac140.sh
+++ rac140.sh
@@ -5,7 +5,7 @@
 set -eu
 
 cat <<EOT >> /etc/udev/rules.d/99-oracle-asmdevices.rules
-KERNEL=="sd?1", SUBSYSTEM=="block", PROGRAM=="/usr/lib/udev/scsi_id -g -u -d /dev/\$parent", RESULT=="`/usr/lib/udev/scsi_id -g -u -d /dev/sdb`", SYMLINK+="oracleasm/asm-disk1", OWNER="oracle", GROUP="dba", MODE="0660"
+KERNEL=="sd?1", SUBSYSTEM=="block", PROGRAM=="/usr/lib/udev/scsi_id -g -u -d /dev/\$parent", RESULT=="`/usr/lib/udev/scsi_id -g -u -d /dev/sdb`", SYMLINK+="oracleasm/asm-disk1", OWNER="oracle", GROUP="asmadmin", MODE="0660"
 EOT
 
 date '+%Y%m%d.%H%M%S' >> ${0##*/}.done
